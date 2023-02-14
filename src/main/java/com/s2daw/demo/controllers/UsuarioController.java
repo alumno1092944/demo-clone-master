a46c28d30a77d55e1680f1c4b8dc6148bba6deea
adri <alumno.1092944@ies-azarquiel.es>
2023-02-14 13:22:03 +0100

    Hasta minuto 1:55:00 del curso. Modifica usuarios.js para que consuma backend mediante fetch. Modifica tables.html y usuarios.js para preparar la carga de usuarios desde el backend

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,35 @@ public class UsuarioController {
         usuario.setEmail("lucasmoy@hotmail.com");
         usuario.setTelefono("234234234");
         return usuario;
+    }
+    @RequestMapping(value="usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("234234234");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("Lucas");
+        usuario2.setApellido("Moy");
+        usuario2.setEmail("lucasmoy@hotmail.com");
+        usuario2.setTelefono("234234234");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("Lucas");
+        usuario3.setApellido("Moy");
+        usuario3.setEmail("lucasmoy@hotmail.com");
+        usuario3.setTelefono("234234234");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
     @RequestMapping(value="usuario")
     public Usuario editar() {

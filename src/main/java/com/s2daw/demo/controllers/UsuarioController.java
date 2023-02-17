e0adfbabf6aee57fad3fb53cfcba2b1dcf569f4e
adri <alumno.1092944@ies-azarquiel.es>
2023-02-17 01:44:23 +0100

    Hasta minuto 03:07:00 del curso ( se añade la opción simplificada de registrar usuario, se corrige error en usuario.java, falta @GeneratedValue, se corrige error en BD para que admita not null telefono)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -3,10 +3,7 @@ package com.s2daw.demo.controllers;
 import com.s2daw.demo.dao.UsuarioDao;
 import com.s2daw.demo.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,12 +24,18 @@ public class UsuarioController {
         return usuario;
     }
 
+    @RequestMapping(value="api/usuarios",method= RequestMethod.GET)
+    public List<Usuario> getUsuarios(){
+        return usuarioDao.getUsuarios();
+    }
+
+    @RequestMapping(value="api/usuarios",method= RequestMethod.POST)
+    public void registraUsuario(@RequestBody Usuario usuario){
+        usuarioDao.registrarUsuario(usuario);
+    }
+
     @RequestMapping(value="api/usuarios/{id}",method= RequestMethod.DELETE)
     public void eliminarUsuario(@PathVariable Long id){
         usuarioDao.eliminarUsuario(id);
-    }
-    @RequestMapping(value="usuarios")
-    public List<Usuario> getUsuarios(){
-        return usuarioDao.getUsuarios();
     }
 }
--- src/main/java/com/s2daw/demo/dao/UsuarioDao.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDao.java
@@ -8,4 +8,6 @@ public interface UsuarioDao {
     List<Usuario> getUsuarios();
 
     void eliminarUsuario(Long id);
+
+    void registrarUsuario(Usuario usuario);
 }

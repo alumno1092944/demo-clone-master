e3285b0fcc092d7b51e203cb759399496d62498f
adri <alumno.1092944@ies-azarquiel.es>
2023-02-21 17:43:00 +0100

    Hasta minuto 03:25:34 del curso (Se añade login de usuario, password se almacena sin aplicar función hash, no hay todavía gestión de inicio de sesión, modificaciones respecto del curso: - fetch cambio const request a response - respuesta de /api/login de text a JSON)

---
+++ src/main/java/com/s2daw/demo/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.s2daw.demo.controllers;
+
+import com.s2daw.demo.dao.UsuarioDao;
+import com.s2daw.demo.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private UsuarioDao usuarioDao;
+    @RequestMapping(value="api/login",method= RequestMethod.POST)
+    public Map loginUsuario(@RequestBody Usuario usuario){
+        String respuesta="FAIL";
+        if (usuarioDao.verificarCredenciales(usuario)) respuesta="OK";
+        return Collections.singletonMap("success", respuesta);
+    }
+
+}
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,23 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    //on ready
+});
+
+async function iniciarSesion(){
+  let datos={};
+  datos.email=document.getElementById('txtEmail').value;
+  datos.password=document.getElementById('txtPassword').value;
+
+  const response = await fetch('api/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(datos)
+  });
+
+    const respuesta = await response.json();
+    if (respuesta.success == 'OK') window.location.href='usuarios.html';
+    else alert('Credenciales incorrectas. Por favor intente nuevamente');
+}
